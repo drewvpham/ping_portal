4e725a4b5af17c147c7521cf40a074e336eaf544
Drew Pham <drewvpham@gmail.com>
2017-06-26 13:07:13 -0500

    more work on user controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user=User.find_by(email: login_params[:email])
     if user && user.authenticate(login_params[:password])
       session[:user_id]=user.id
-      redirect_to '/home'
+      redirect_to users_path
     else
       flash[:login_error]='Invalid credentials'
       redirect_to '/'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,37 @@
 class UsersController < ApplicationController
-  
-  def create
-    @user= User.create(user_params)
-    if @user.valid?
-      session[:user_id]=@user.id
-      redirect_to users_path
-    else
-      flash[:errors]= @user.errors.full_messages
-      redirect_to :back
-    end
+  before_action :set_user, only: [:show, :update, :edit, :delete]
+
+def index
+end
+
+def create
+  @user= User.create(user_params)
+  if @user.valid?
+    session[:user_id]=@user.id
+    redirect_to users_path
+  else
+    flash[:errors]= @user.errors.full_messages
+    redirect_to :back
+  end
+end
+
+def edit
+end
+
+def update
+  @user.update(user_params)
+  redirect_to events_path
+end
+
+def show
+end
+
+private
+  def user_params
+    params.require(:user).permit( :username, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+      @user=User.find(session[:user_id])
   end
 end
