8c74b31d1332926559e851fe03c8bb1d198e8d32
Marshall Alex <r.marshall.alex@gmail.com>
2017-06-28 11:30:44 -0500

    added points 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :messages
   has_many :games, source: :player_1, foreign_key: :player_1_id
+  has_many :wins, through: :games, source: :winner
   validates_presence_of :username, :email
   validates :username, :email, uniqueness: { case_sensitive: false }
 
